8c66c8cb4812996322073ef56a422abf3cdfd591
sikara <xuhaoxian@gdlinkjob.com>
2025-05-22 17:09:56 +0800

    pref Gamekee游戏banner优先返回高清图

--- src/gamekee/util.ts
+++ src/gamekee/util.ts
@@ -4,12 +4,12 @@ import { CalendarActivityResult, GamekeeData } from './DataType';
 export const getShanghaiDate = (date?: dayjs.ConfigType) => dayjs(date).tz('Asia/Shanghai');
 
 export const handleGamekeeEvent = (data: GamekeeData['data']): CalendarActivityResult['data'] => {
-	return data.map(({ picture, link_url, begin_at, end_at, ...item }) => {
+	return data.map(({ picture, big_picture, link_url, begin_at, end_at, ...item }) => {
 		return {
 			...item,
 			start_time: dayjs(begin_at * 1000).format('YYYY-MM-DD HH:mm:ss'),
 			end_time: dayjs(end_at * 1000).format('YYYY-MM-DD HH:mm:ss'),
-			banner: picture,
+			banner: big_picture || picture,
 			linkUrl: link_url,
 		};
 	});
