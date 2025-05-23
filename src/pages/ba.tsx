908b3b10beda98ff1709ce6225ffdace84341382
sikara <987555458@qq.com>
2025-05-23 15:16:16 +0800

    feat 添加蔚蓝档案（国服）

---
+++ src/pages/ba.tsx
@@ -0,0 +1,40 @@
+import EventCalendar from "@/components/EventCalendar";
+import { CalendarActivity } from "@/components/EventCalendar/CalendarType";
+import Loading from "@/components/Loading";
+import { BACKEND_URL } from "@/services/note";
+import axios from "axios";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+function Ba() {
+  const [eventData, setEventData] = useState<CalendarActivity[]>();
+  const { isFetching } = useQuery({
+    queryKey: ["ba"],
+    queryFn: () => axios(`${BACKEND_URL}/ba`),
+    onSuccess(data: any) {
+      setEventData(
+        data?.data?.data?.filter((item: CalendarActivity) => {
+          return Boolean(item.start_time);
+        })
+      );
+    },
+  });
+  // 检验日历组件
+  return (
+    <>
+      {isFetching ? (
+        <Loading />
+      ) : (
+        <EventCalendar
+          {...{
+            value: dayjs(),
+            activity: eventData ?? [],
+          }}
+        />
+      )}
+    </>
+  );
+}
+
+export default Ba;
