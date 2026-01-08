2518611b07bc8e395059990eaff392adbb25a050
sikara <987555458@qq.com>
2026-01-08 15:16:38 +0800

    fix(Arknights):完善对于跨年活动前后年份获取逻辑

--- apps/cloudflare-backend/src/Arknights/util.ts
+++ apps/cloudflare-backend/src/Arknights/util.ts
@@ -39,7 +39,7 @@ const getAKEventDetail = async (url: string) => {
 			}
 			const tmp = html?.find((str) => str.includes(title));
 			const banner = (tmp && getImgBanner(tmp)) || '';
-			return { id: `${data.cid}${i}`, title, start_time, end_time, banner, linkUrl: data.jumpLink };
+			return { id: `${data.cid}${i}`, title, start_time, end_time, banner, linkUrl: data.jumpLink, displayTime: data.displayTime };
 		}) ?? [];
 	return event?.filter((item) => item !== null);
 };
@@ -48,26 +48,41 @@ const parseStrToTime = (str: string, timeCalibrationVal: string) => {
 	let [start_time_str, end_time_str] = str.replace(/活动时间：/, '').split('-');
 	let [start_time, end_time] = [start_time_str, end_time_str]
 		.map((s) => s.trim())
+		// (2025 年)? 12 月 11 日 16:10 这类格式都能识别
 		.map((item) => (item ? dayjs(item, ['YYYY MM DD HH:mm', 'MM DD HH:mm', 'MM DD'], 'zh-cn') : null));
 
 	const publishTime = dayjs(timeCalibrationVal);
 	if (!start_time || !end_time) return undefined;
-	if (!start_time_str.includes('年')) {
-		start_time = start_time.year(publishTime.year());
-		// 对于publishTime是12月发布的，而start_time是1月，则start_time要加1年
-		if (publishTime.diff(start_time, 'month') > 1) {
-			start_time = start_time.add(1, 'year');
-		}
-	}
 	if (!start_time_str.includes(':')) {
 		start_time = start_time.hour(16);
 	}
 	if (!end_time_str.includes(':')) {
 		end_time = end_time.hour(4);
 	}
-	if (start_time.isAfter(end_time)) {
-		end_time = end_time.add(1, 'year');
+	if (!str.includes('年')) {
+		// 如果还能显示前年年尾活动，需要根据帖子发布时间调整
+		if (publishTime.year() !== start_time.year()) {
+			start_time = start_time.year(publishTime.year());
+			end_time = end_time.year(publishTime.year());
+		}
+		return [start_time.format(TIME_FORMAT), end_time.format(TIME_FORMAT)];
 	}
+	// 对于跨年活动特殊处理
+	// 大概格式：12月23日 16:10 - 2025年01月2日 4:10
+	// 在本地时间25年时候， start_time 要减1年
+	if (!start_time_str.includes('年') && end_time_str.includes('年')) {
+		if (start_time.isAfter(end_time)) {
+			start_time = start_time.add(-1, 'year');
+		}
+	}
+	// 对于 2025年01月03日 16:10 - 01月12日 4:10 这类活动
+	// 在本地时间还是24年时候，end_time 要加1年
+	if (start_time_str.includes('年') && !end_time_str.includes('年')) {
+		if (start_time.isAfter(end_time)) {
+			end_time = end_time.add(1, 'year');
+		}
+	}
+
 	return [start_time.format(TIME_FORMAT), end_time.format(TIME_FORMAT)];
 };
 
